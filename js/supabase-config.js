f75a879c73b1c66fdcf34c89c29dda43ab8f14fb
404BrainNotFound <u2m0a1r3ktk@gmail.com>
2026-07-23 18:00:00 +0500

    Connect IdeaCanvas to Supabase

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
 window.IDEACANVAS_SUPABASE = {
-  url: "",
-  publishableKey: "",
+  url: "https://epjlxqggvdyafznurfqm.supabase.co",
+  publishableKey: "sb_publishable_f0TvabQATo2w5TDwFNR74A_NRazX5gV",
 };
